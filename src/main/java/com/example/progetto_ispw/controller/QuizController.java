47932e923b5a63c50e7ee9ff9e8a6285e0da07b4
OrsoWojtek <ale.bru132@gmail.com>
2025-01-20 18:03:10 +0100

    54 commit: primo tentativo di sistemare i code smells e la duplicazione dovuta alle nuove classi inserite

--- src/main/java/com/example/progetto_ispw/controller/QuizController.java
+++ src/main/java/com/example/progetto_ispw/controller/QuizController.java
@@ -4,13 +4,14 @@ import com.example.progetto_ispw.Connessione;
 import com.example.progetto_ispw.bean.QuizInfoBean;
 import com.example.progetto_ispw.exception.ConnectionException;
 import com.example.progetto_ispw.exception.DataNotFoundException;
+import com.example.progetto_ispw.model.sessione.Session;
 import com.example.progetto_ispw.model.sessione.SessionManager;
 
 //----CONTROLLER APPLICATIVO PER GESTIRE IL QUIZ----
 public class QuizController {
     //----METODO PER RESTITUIRE LE INFO DEL QUIZ CORRENTE
     public QuizInfoBean getInfoQuiz() throws DataNotFoundException {
-        return SessionManager.getInstance().getSession("quiz_page").getDato("quiz",QuizInfoBean.class);
+        return getQuizSession().getDato("quiz",QuizInfoBean.class);
     }
     //----METODO PER PULIRE CONNESSIONE AL DB E SESSIONE AL LOGOUT----
     public void clean() throws ConnectionException {
@@ -20,12 +21,17 @@ public class QuizController {
     }
     //----METODO PER PULIRE LE SESSIONI SUCCESSIVE A QUELLA DI LOGIN----
     public void clearOtherInfo(){
-        SessionManager istance = SessionManager.getInstance();
-        istance.getSession("quiz_page").removeDato("quiz");
-        istance.getSession("course_page").removeDato("corso");
+        clearInfoQuiz();
+        SessionManager.getInstance().getSession("course_page").removeDato("corso");
     }
     //----METODO PER RESETTARE NELLA SESSIONE LE INFO SUL QUIZ SELEZIONATO
     public void clearInfoQuiz(){
-        SessionManager.getInstance().getSession("quiz_page").removeDato("quiz");
+        getQuizSession().removeDato("quiz");
+    }
+
+
+    //----METODO PER OTTENERE LA SESSIONE RIGUARDO ALLA PAGINA DEL QUIZ
+    private Session getQuizSession(){
+        return SessionManager.getInstance().getSession("quiz_page");
     }
 }
