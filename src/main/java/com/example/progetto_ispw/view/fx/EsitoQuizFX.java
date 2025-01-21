4b10bcb295ec4189114eb0e0023573d5f21256c0
OrsoWojtek <ale.bru132@gmail.com>
2025-01-21 18:40:43 +0100

    61 commit: accennato pagina per l'esito del quiz (fxml e fx) e collegato il tasto Termina al suo metodo nel 2° controller fx riguardante il quiz

---
+++ src/main/java/com/example/progetto_ispw/view/fx/EsitoQuizFX.java
@@ -0,0 +1,7 @@
+package com.example.progetto_ispw.view.fx;
+
+import com.example.progetto_ispw.view.PageManager;
+
+//----CONTROLLER GRAFICO SECONDO IL PATTERN MVC PER LA GESTIONE DELLE INTERAZIONI DELL'UTENTE CON IL SISTEMA [CASO SPECIFICO: QUIZ (3/3)]----
+public class EsitoQuizFX extends PageManager {
+}
