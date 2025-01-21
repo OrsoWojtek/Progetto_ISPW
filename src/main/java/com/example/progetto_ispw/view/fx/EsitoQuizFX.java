cf7965aabc8eb4fa3e3771308e5ca75c40bfaf75
OrsoWojtek <ale.bru132@gmail.com>
2025-01-22 00:06:44 +0100

    65 commit:Creata la pagina di esito (non funzionano il tasto home e logout)

--- src/main/java/com/example/progetto_ispw/view/fx/EsitoQuizFX.java
+++ src/main/java/com/example/progetto_ispw/view/fx/EsitoQuizFX.java
@@ -1,7 +1,49 @@
 package com.example.progetto_ispw.view.fx;
 
+import com.example.progetto_ispw.controller.QuizController;
+import com.example.progetto_ispw.exception.ConnectionException;
+import com.example.progetto_ispw.exception.PageNotFoundException;
 import com.example.progetto_ispw.view.PageManager;
+import javafx.fxml.FXML;
 
 //----CONTROLLER GRAFICO SECONDO IL PATTERN MVC PER LA GESTIONE DELLE INTERAZIONI DELL'UTENTE CON IL SISTEMA [CASO SPECIFICO: QUIZ (3/3)]----
 public class EsitoQuizFX extends PageManager {
+    private QuizController controllerQuiz; //Riferimento al controller applicativo
+
+    @FXML
+    public void initialize() {
+        controllerQuiz = new QuizController();
+    }
+    //----METODO PER EFFETTUARE IL LOGOUT----
+    @FXML
+    public void onLogoutButtonClicked(){
+        try {
+            controllerQuiz.clean();
+            pageLoader.loadPage("login");
+        } catch (PageNotFoundException e){
+            showMessageHandler.showError(e.getMessage(),"Pagina non trovata");
+        } catch (ConnectionException e){
+            showMessageHandler.showError(e.getMessage(),"Errore connessione");
+        }
+    }
+    //----METODO PER TORNARE ALLA HOME----
+    @FXML
+    public void onHomeButtonClicked(){
+        controllerQuiz.clearOtherInfo();
+        try{
+            pageLoader.loadPage("home");
+        } catch (PageNotFoundException e){
+            showMessageHandler.showError(e.getMessage(),"Pagina non trovata");
+        }
+    }
+    //----METODO PER TORNARE ALLA PAGINA DEL CORSO----
+    @FXML
+    public void onTornaAlCorsoClicked(){
+        controllerQuiz.clearInfoQuiz();
+        try{
+            pageLoader.loadPage("corso");
+        } catch (PageNotFoundException e){
+            showMessageHandler.showError(e.getMessage(),"Pagina non trovata");
+        }
+    }
 }
