4cd183b81ea518ecdb408f4984c1970763d398cf
OrsoWojtek <ale.bru132@gmail.com>
2025-01-21 18:24:20 +0100

    59 commit: modifica al messaggeHandler con l'aggiunta del metodo askConfirmation

--- src/main/java/com/example/progetto_ispw/view/ShowMessageHandler.java
+++ src/main/java/com/example/progetto_ispw/view/ShowMessageHandler.java
@@ -1,6 +1,7 @@
 package com.example.progetto_ispw.view;
 
-public interface ShowMessageHandler { //Interfaccia per modellare le classi che si occupano del display dei messaggi
+public interface ShowMessageHandler { //Interfaccia per modellare le classi che si occupano del display dei messaggi e delle richieste di conferma all'utente
     void showError(String message, String title);
     void showMessage(String message, String title);
+    boolean askConfirmation(String header, String content, String title);
 }
--- src/main/java/com/example/progetto_ispw/view/fx/ShowMessageHandlerFX.java
+++ src/main/java/com/example/progetto_ispw/view/fx/ShowMessageHandlerFX.java
@@ -2,6 +2,10 @@ package com.example.progetto_ispw.view.fx;
 
 import com.example.progetto_ispw.view.ShowMessageHandler;
 import javafx.scene.control.Alert;
+import javafx.scene.control.ButtonType;
+
+import java.util.Optional;
+
 //----CLASSE PER GESTIRE I MESSAGGI DI ERRORE DA MOSTRARE A SCHERMO (VIEW: JAVAFX)
 public class ShowMessageHandlerFX implements ShowMessageHandler {
     @Override
@@ -23,5 +27,16 @@ public class ShowMessageHandlerFX implements ShowMessageHandler {
 
         //Mostra il popup e attendi la chiusura
         alert.showAndWait();
+    }
+    @Override
+    public boolean askConfirmation(String header, String content, String title){
+        //Mostra una finestra di dialogo di conferma
+        Alert alert = new Alert(Alert.AlertType.CONFIRMATION);
+        alert.setTitle(title);
+        alert.setHeaderText(header);
+        alert.setContentText(content);
+
+        Optional<ButtonType> result = alert.showAndWait(); //Mostra il dialogo e aspetta la risposta dell'utente
+        return result.isPresent() && result.get() == ButtonType.OK; //Se l'utente ha premuto "OK" (conferma) ritorna 'true', mentre se ha premuto "Annulla" o ha chiuso la finestra ritorna 'false'
     }
 }
